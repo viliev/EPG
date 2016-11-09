961ff2765ffbddc05c292e1e19dbe85b39ee48aa
Harry G <harrygg@gmail.com>
2016-11-09 15:11:37 +0200

    removed extra files

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-cd /home/hgenev/EPG/
-
+cd ~/EPG/
+#pull recent updates
 git pull
 
+#Run the collector
+cd ~/epg-collector/
 ./generate.py
 
-git pull
+cp ./alltv-guide.xml ~/EPG/epg.xml
+cp ./alltv-guide.xml.gz ~/EPG/epg.xml.gz
+
+
+#commit EPG update
+cd ~/EPG/
 git add -A
 git commit -m "Regular EPG update"
 git push
