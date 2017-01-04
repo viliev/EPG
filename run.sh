b71eece8de1d514be192246a3191974298fe437e
Harry G <harrygg@gmail.com>
2017-01-04 17:02:25 +0200

    Added validation output

--- run.sh
+++ run.sh
@@ -15,10 +15,12 @@ echo "Using argument=$arg"
 ./validate.py
 
 cp ./epg.xml ~/EPG/
+cp ./validation.json ~/EPG/
 cd ~/EPG/
 
 gzip < epg.xml > epg.xml.gz
 gzip < epg.xml > alltv-guide.xml.gz
+
 
 #md5sum epg.xml > checksum.txt
 cut -d ' ' -f 1 <<< `md5sum epg.xml` > checksum.txt
